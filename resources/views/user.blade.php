ee3ed956af3efe1c3be340d4a349aefc2c2a6fee
knoexus <knoexus@yahoo.com>
2020-12-23 12:41:34 +0300

    basic user styling

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,8 +1,32 @@
 @extends('layouts.app')
 
+@section('styles')
+    <link rel="stylesheet" href="{{ asset('css/user.css') }}">
+@endsection
+
 @section('content')
-<h3>{{ $user->first_name }} {{ $user->last_name }}</h3>
-<img src="/storage/{{ $user->profile->image }}" alt="profile Pic" height="200" width="200"/>
-<a class="btn btn-outline-secondary" href="/user/{{ Auth::user()->id }}/journey/create" role="button">Share new journey</a>
+<div class="container mt-3">
+    <div class="user-main_info">
+        <div class="d-flex">
+            <div>
+                <img class="user-picture" src="/storage/{{ $user->profile->image }}" alt="profile Pic" height="200" width="200"/>
+            </div>
+            <div class="ml-4 d-flex flex-column">
+                <div class="mt-4">
+                    <h3>{{ $user->first_name }} {{ $user->last_name }}</h3>
+                    <h4 class="user-username">{{ $user->user_name }}</h4>
+                </div>
+            </div>
+            <div class="ml-4">
+                @if (Auth::user()->id == $user->id)
+                    <a class="btn btn-outline-info" href="/user/{{ Auth::user()->id }}/edit" role="button">Edit profile</a>
+                @endif
+            </div>
+        </div>
+    </div>
+    <div class="posts mt-4">
+        <a class="btn btn-outline-secondary" href="/user/{{ Auth::user()->id }}/journey/create" role="button">Share new journey</a>
+    </div>
+</div>
 @endsection
 
